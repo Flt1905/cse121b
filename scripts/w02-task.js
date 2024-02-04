a5e8e2974d98dc502685a5c5257d754bc293d74c
Flt1905 <ftshilande@icloud.com>
2024-02-04 08:28:25 +0200

    task 2

---
+++ scripts/w02-task.js
@@ -0,0 +1,43 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+
+const fullName = "Fulu Tshilande";
+
+const currentYear = new Date().getFullYear();
+/*console.log(currentYear);*/
+
+const profilePicture = "images/profile.webp";
+
+
+
+/* Step 3 - Element Variables */
+
+//const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('#food');
+const yearElement = document.querySelector('#year');
+const imageElement = document.querySelector('img');
+
+// /* Step 4 - Adding Content */
+
+// nameElement.innerHTML = <strong> ${fullName}</strong>;
+yearElement.textContent = currentYear;
+imageElement.setAttribute('src', profilePicture);
+imageElement.setAttribute('alt', `This is ${fullName}`);
+
+/* Step 5 - Array */
+
+let  lovedFoods = ['fruits', 'Cheese','rice','Meat'];
+const food = document.getElementById('lovedFoods');
+
+foodElement.innerHTML = lovedFoods;
+let newfood = 'orange'
+lovedFoods.push(newfood);
+foodElement.innerHTML+= `<br>${lovedFoods}`
+// console.log (food);
+
+
+
+
