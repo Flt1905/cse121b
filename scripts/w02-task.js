4721ae4b3e0d9a47eb02b3d8b95c0f0b32c03c3b
Flt1905 <ftshilande@icloud.com>
2024-02-04 08:52:30 +0200

    image updated

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -15,14 +15,14 @@ const profilePicture = "images/profile.webp";
 
 /* Step 3 - Element Variables */
 
-const nameElement = document.getElementById('#name');
+//onst nameElement = document.getElementById('#name');
 const foodElement = document.getElementById('#food');
-const yearElement = document.querySelector('#year');
+const yearElement = document.querySelector("#year");
 const imageElement = document.querySelector('img');
 
 // /* Step 4 - Adding Content */
 
-nameElement.innerHTML = <strong> ${fullName}</strong>;
+//nameElement.innerHTML = <strong> ${fullName}</strong>;
 yearElement.textContent = currentYear;
 imageElement.setAttribute('src', profilePicture);
 imageElement.setAttribute('alt', `This is ${fullName}`);
